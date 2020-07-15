c261c9efc9990c659adc7d9a4fa3c7dd56bd5204
Faten RIAHI <49404590+RiahiFaten@users.noreply.github.com>
2020-07-15 18:28:18 +0200

    Update Serveur2.c

--- Serveur2.c
+++ Serveur2.c
@@ -26,7 +26,7 @@ int interrogation_bd(char* id, char table_response[MAX*MAX]) {
         fprintf(stderr, "%s\n" ,mysql_error(con));
         exit(-1);
     }
-    if (mysql_real_connect(con,"localhost","faten","facturesdbpw","fatendb",0,NULL,0)==NULL) {
+    if (mysql_real_connect(...)==NULL) {
         fprintf(stderr,"%s\n",mysql_error(con));
         mysql_close(con);
         exit(1);
